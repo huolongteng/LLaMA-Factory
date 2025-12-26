b4c40d058fbc6df37d97d552a56a654319eed5f5
HUO LONGTENG-Leo <hlt@bistu.edu.cn>
2025-12-26 22:10:48 +0800

    更新 gkd_train_models.py

--- gkd_train_models.py
+++ gkd_train_models.py
@@ -1,52 +1,48 @@
+from trl.experimental.gold import GOLDConfig, GOLDTrainer
 from datasets import load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from trl.experimental.gkd import GKDConfig, GKDTrainer
 
-
-tokenizer = AutoTokenizer.from_pretrained("GreatGoose/gemma3-4b-it-lora-loglm")
-# The model to optimise
-model = AutoModelForCausalLM.from_pretrained("GreatGoose/gemma3-270m-full-loglm")
-model.resize_token_embeddings(len(tokenizer))
-# The teacher model to calculate the KL divergence against
-teacher_model = AutoModelForCausalLM.from_pretrained("GreatGoose/gemma3-4b-it-lora-loglm")
-teacher_model.resize_token_embeddings(len(tokenizer))
-
+# Create dateset.
 raw_data_path = "data/train_gemma3.jsonl"
-ds = load_dataset("json", data_files={"train": raw_data_path})
-split = ds["train"].train_test_split(test_size=0.2, seed=42)
 
-train_dataset = split["train"]
-eval_dataset = split["test"]
+dataset = load_dataset("json", data_files=raw_data_path, split="train[:80%]")
 
-def convert_to_chatml(example):
-    new_messages = []
-    for msg in example["messages"]:
-        if msg["from"] == "human":
-            role = "user"
-        elif msg["from"] == "gpt":
-            role = "assistant"
-        else:
-            raise ValueError(f"Unknown role: {msg['from']}")
+def to_chatml(example):
+    return {
+        "messages": [
+            {
+                "role": "user" if m["from"] == "human" else "assistant",
+                "content": m["value"]
+            }
+            for m in example["messages"]
+        ]
+    }
 
-        new_messages.append({
-            "role": role,
-            "content": msg["value"]
-        })
+train_dataset = dataset.map(to_chatml, remove_columns=dataset.column_names)
 
-    return {"messages": new_messages}
+# Load model
+student_name = "GreatGoose/gemma3-270m-full-loglm"
+teacher_name = "GreatGoose/gemma3-4b-it-lora-loglm"
 
-train_dataset = train_dataset.map(convert_to_chatml, remove_columns=train_dataset.column_names)
-eval_dataset  = eval_dataset.map(convert_to_chatml,  remove_columns=eval_dataset.column_names)
+model = AutoModelForCausalLM.from_pretrained(student_name)
+teacher_model = AutoModelForCausalLM.from_pretrained(teacher_name)
 
 
-training_args = GKDConfig(output_dir="gkd-model", per_device_train_batch_size=1)
-trainer = GKDTrainer(
+training_args = GOLDConfig(
+    output_dir="gold-model",
+    per_device_train_batch_size=1,
+    teacher_model=teacher_name,
+    teacher_tokenizer_name_or_path=teacher_name,
+    use_uld_loss=True,
+    uld_use_hybrid_loss=True,
+)
+
+trainer = GOLDTrainer(
     model=model,
     teacher_model=teacher_model,
     args=training_args,
     processing_class=tokenizer,
     train_dataset=train_dataset,
-    eval_dataset=eval_dataset,
 )
-
 trainer.train()
+
