15641464a716fbc9cb2daf38940be5bce5d8007c
huolongteng <hlt@bistu.edu.cn>
2025-12-26 20:23:03 +0800

    Refactor dataset loading and add conversion function for ChatML format

--- gkd_train_models.py
+++ gkd_train_models.py
@@ -2,11 +2,15 @@ from datasets import load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from trl.experimental.gkd import GKDConfig, GKDTrainer
 
-tokenizer = AutoTokenizer.from_pretrained("GreatGoose/gemma3-270m-full-loglm-4bit")
+
+tokenizer = AutoTokenizer.from_pretrained("GreatGoose/gemma3-4b-it-lora-loglm")
 # The model to optimise
-model = AutoModelForCausalLM.from_pretrained("GreatGoose/gemma3-270m-full-loglm-4bit")
+model = AutoModelForCausalLM.from_pretrained("GreatGoose/gemma3-270m-full-loglm")
+model.resize_token_embeddings(len(tokenizer))
 # The teacher model to calculate the KL divergence against
 teacher_model = AutoModelForCausalLM.from_pretrained("GreatGoose/gemma3-4b-it-lora-loglm")
+teacher_model.resize_token_embeddings(len(tokenizer))
+
 raw_data_path = "data/train_gemma3.jsonl"
 ds = load_dataset("json", data_files={"train": raw_data_path})
 split = ds["train"].train_test_split(test_size=0.2, seed=42)
@@ -34,6 +38,7 @@ def convert_to_chatml(example):
 train_dataset = train_dataset.map(convert_to_chatml, remove_columns=train_dataset.column_names)
 eval_dataset  = eval_dataset.map(convert_to_chatml,  remove_columns=eval_dataset.column_names)
 
+
 training_args = GKDConfig(output_dir="gkd-model", per_device_train_batch_size=1)
 trainer = GKDTrainer(
     model=model,
@@ -43,4 +48,5 @@ trainer = GKDTrainer(
     train_dataset=train_dataset,
     eval_dataset=eval_dataset,
 )
+
 trainer.train()
